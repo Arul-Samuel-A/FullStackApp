eca0f6ebe0ed49769de1291b5f4e1a285f7e5db0
Arul-Samuel-A <arulsamuela@gmail.com>
2025-06-05 17:32:48 +0530

    Final commit before Render deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const axiosInstance = axios.create({
-    baseURL:"http://localhost:5000/api",
+    baseURL:"https://bookshelf-5p0s.onrender.com/api",
     withCredentials: true,
 })
 
